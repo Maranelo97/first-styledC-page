037dd295ea0b5ee7c2d2a24d1f79dbe9962e602d
Mariano Santos <marianoveronsantos@gmail.com>
2022-11-11 17:18:36 -0300

    first commit

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { Button, Container, MainHeading } from '../../globalStyles';
 import Navbar from "../NavBar/NavBar";
 import { HeroVideo, HeroSection, HeroText, ButtonWrapper, HeroButton } from './HeroStyles';
